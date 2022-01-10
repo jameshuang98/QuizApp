00633d32c0acdc81765a4019100966373fdaabde
Amal Botan <amalbotan254@gmail.com>
2022-01-10 17:59:45 +0000

    Updated quizzes homepage, added quiz route (not done), and added template for new quiz page

---
+++ routes/quizzes.js
@@ -0,0 +1,28 @@
+/*
+ * All routes for quizzes are defined here
+ * Since this file is loaded in server.js into api/widgets,
+ *   these routes are mounted onto /widgets
+ * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
+ */
+
+const express = require('express');
+const router  = express.Router();
+
+module.exports = (db) => {
+  router.get("/quizzes", (req, res) => {
+    let query = `SELECT name FROM quizzes
+    WHERE quizzes.public IS true;`;
+    console.log(query);
+    db.query(query)
+      .then(data => {
+        const quizzes = data.rows;
+        res.json({ quizzes });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+  return router;
+};
--- server.js
+++ server.js
@@ -37,11 +37,13 @@ app.use(express.static("public"));
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
 const widgetsRoutes = require("./routes/widgets");
+const quizzesRoutes = require("./routes/quizzes");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
+app.use("/api/quizzes", quizzesRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
 // Home page
@@ -50,6 +52,10 @@ app.use("/api/widgets", widgetsRoutes(db));
 
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+app.get("/quizzes/new", (req, res) => {
+  res.render("new_quiz");
 });
 
 app.listen(PORT, () => {
