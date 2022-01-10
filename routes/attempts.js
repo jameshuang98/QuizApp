4086ff163368236d94c51633500dc1cc570bddb1
Lucy Shen <lucyshen77@gmail.com>
2022-01-10 18:47:34 +0000

    added query for results of recent attempt in queries/04_results_of_recent_attempt.sql, added margins to .public_quizzes in main.scss file, created routes/attempts.js for attempts

--- routes/attempts.js
+++ routes/attempts.js
@@ -1,7 +1,7 @@
 /*
- * All routes for Users are defined here
- * Since this file is loaded in server.js into api/users,
- *   these routes are mounted onto /users
+ * All routes for Widgets are defined here
+ * Since this file is loaded in server.js into api/widgets,
+ *   these routes are mounted onto /widgets
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
@@ -10,10 +10,13 @@ const router  = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
-    db.query(`SELECT * FROM users`)
+    let query = `SELECT * FROM attempts`;
+    console.log(query);
+    db.query(query)
       .then(data => {
-        const users = data.rows;
-        res.json({ users });
+        const attempts = data.rows;
+        console.log(attempts);
+        res.json({ attempts });
       })
       .catch(err => {
         res
